8ea2ce60bda4e3794f96ff61125b9d31d0b36e64
Gaurav Negi <gauravnegi6987@gmail.com>
2025-07-18 00:58:15 +0530

    link added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
         <img width={150} src={assets.logo} alt="" />
-        <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @GreatStack.dev | All right reserved.</p>
+        <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright GAURAV NEGI | All right reserved.</p>
         <div className='flex gap-2.5'>
-            <img width={35} src={assets.facebook_icon} alt="" />
-            <img width={35} src={assets.twitter_icon} alt="" />
-            <img width={35} src={assets.instagram_icon} alt="" />
+            <a href="https://www.facebook.com/profile.php?id=100054396558351" target='_blank' rel='noopenernoreferrer'><img width={35} src={assets.facebook_icon} alt="" /></a>
+            <a href="#" target='_blank' rel='noopenernoreferrer'><img width={35} src={assets.twitter_icon} alt="" /></a>
+            <a href="https://www.instagram.com/_gaurav_negi_/" target='_blank' rel='noopenernoreferrer'><img width={35} src={assets.instagram_icon} alt="" /></a>
         </div>
     </div>
   )
